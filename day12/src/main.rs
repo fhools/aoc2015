007d38d10d12bb11718095352797b25667fbefce
fhools <fhools@gmail.com>
2021-05-05 22:41:49 -0700

    refactor day12

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,8 +1,9 @@
+use serde_json::Value;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
-use serde_json::{Result, Value};
 const PART: u8 = 2;
+
 fn sum_nums(val: &Value) -> f64 {
     let mut sum = 0.0;
     match val {
@@ -13,45 +14,41 @@ fn sum_nums(val: &Value) -> f64 {
             // a key with value "red"
             if PART == 2 {
                 for v in map.values() {
-                    if let Value::String(ref s) =  v {
+                    if let Value::String(ref s) = v {
                         if s == "red" {
                             foundred = true;
                             break;
                         }
-                    }                 
+                    }
                 }
             }
-            for v in map.values() {
-                if PART == 1 || (PART == 2 && !foundred) {
+            if PART == 1 || (PART == 2 && !foundred) {
+                for v in map.values() {
                     sum += sum_nums(v);
                 }
             }
-        },
+        }
 
         Value::Array(ref arr) => {
             for a in arr {
                 sum += sum_nums(a);
             }
-        },
+        }
 
         Value::Number(ref num) => {
             sum += num.as_f64().unwrap();
-        },
-
-        _ => {
-
         }
 
+        _ => {}
     }
     sum
 }
-fn main()  {
-    
+fn main() {
     let fh = File::open("input.txt").unwrap();
     let mut reader = BufReader::new(fh);
     let mut data = String::new();
-    if let _ = reader.read_to_string(&mut data) {
+    if let Ok(_) = reader.read_to_string(&mut data) {
         let v: Value = serde_json::from_str(&data).unwrap();
-        println!("sum: {}", sum_nums(&v));
+        println!("sum of all numbers: {}", sum_nums(&v));
     }
 }
