81991e53e86120c8eda6e086a50fa4bcb3ed633a
fhools <fhools@gmail.com>
2021-05-07 20:36:31 -0700

    Add some comments to day16 code

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -36,19 +36,22 @@ impl Aunty {
         self.props[*keyid as usize] = Some(val);
     }
 
-    fn is_target(&self, target: &Vec<i32>) -> bool {
-        for i in 0..target.len() {
+    fn is_target(&self, target_props: &Vec<i32>) -> bool {
+        for i in 0..target_props.len() {
             if let Some(val) = self.props[i] {
+                // cats, trees should be greater than
                 if (PART == 2) && (i == 1 || i == 7) {
-                    if val <= target[i]  {
+                    if val <= target_props[i]  {
                         return false;
                     }
                 } else if (PART == 2) && (i == 3 || i == 6) {
-                    if val >= target[i] {
+                // pomeranians, goldfishes should be less than
+                    if val >= target_props[i] {
                         return false;
                     }
+                // all others should be equal to target property
                 } else {
-                    if val != target[i] {
+                    if val != target_props[i] {
                         return false;
                     }
                 }
@@ -82,7 +85,9 @@ fn parseline(s: &String, prop2id: &HashMap<String,i32>) -> Aunty {
 }
 fn main() {
     let mut prop2id : HashMap<String, i32> = HashMap::new();
-    
+    // We're mapping each property to an id so we can iterate through
+    // an array of properties. 
+    // Create the key to id map here.
     prop2id.insert("children".to_string(), 0);
     prop2id.insert("cats".to_string(), 1);
     prop2id.insert("samoyeds".to_string(), 2);
@@ -94,22 +99,13 @@ fn main() {
     prop2id.insert("cars".to_string(), 8);
     prop2id.insert("perfumes".to_string(), 9);
 
-    /* children: 3
-    cats: 7
-    samoyeds: 2
-    pomeranians: 3
-    akitas: 0
-    vizslas: 0
-    goldfish: 5
-    trees: 3
-    cars: 2
-    perfumes: 1 */
-    let target = vec![3, 7, 2, 3, 0, 0, 5, 3, 2, 1];
+    // Values we are looking for for each property
+    let target_props = vec![3, 7, 2, 3, 0, 0, 5, 3, 2, 1];
    
     let lines = read_file("input.txt").unwrap();
     let aunty : Vec<&String> = lines.iter().filter(|l| {
         let aunty = parseline(l, &prop2id);
-        aunty.is_target(&target)
+        aunty.is_target(&target_props)
     }).collect();
     println!("aunty: {:?}", aunty);
 }
